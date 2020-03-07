8721798305f179e1ec5e0189df4938d315d17e16
Jamail <j.lavan@hotmail.com>
2020-03-07 07:03:01 -0800

    Toolbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React, { Component} from 'react';
-import './App.css';
+import React, { Component } from 'react';
 import Layout from './components/Layout/Layout'
 import BurgerBUilder from './containers/BurgerBuilder/BurgerBuilder'
 
 
-function App() {
-  return (
-    <div className="App">
-      <Layout>
-        <BurgerBUilder/>
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <BurgerBUilder />
 
-      </Layout>
-      
-    </div>
-  );
+        </Layout>
+
+      </div>
+    );
+  }
 }
 
 export default App;
